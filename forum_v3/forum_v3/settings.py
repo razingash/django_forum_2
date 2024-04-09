44134d2a738fadd0dfca78d8b20d0419b618c658
raiz <128097369+razingash@users.noreply.github.com>
2024-04-09 12:41:09 +0200

    added simple nginx example with gunicorn via TCP connection

--- forum_v3/forum_v3/settings.py
+++ forum_v3/forum_v3/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG') == 'True'
 
 SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = False
@@ -36,7 +36,7 @@ SECURE_HSTS_PRELOAD = False
 SECURE_CONTENT_TYPE_NOSNIFF = False
 
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 AUTH_USER_MODEL = 'forum.CustomUser'
 # Application definition
